98ac526c8fbee4b6e224a4c21a9fd254320fc18b
Kyle Plummer <kyle.plummer@revature.com>
2025-11-02 22:08:13 -0500

    new cors filter

--- revagenda-server/src/main/java/com/revature/revagenda_server/configs/CorsConfig.java
+++ revagenda-server/src/main/java/com/revature/revagenda_server/configs/CorsConfig.java
@@ -5,23 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+
 
 @Configuration
 public class CorsConfig {
-    public CorsConfig() {
-        System.out.println("CorsConfig initialized");
-    }
 
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
-        System.out.println("CorsFilter bean created");
-        return new CorsFilter(source);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
